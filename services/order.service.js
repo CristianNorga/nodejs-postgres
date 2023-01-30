a936b6f362d3b7d0b165ee33174ee987dd7a326b
Cristian Noreña <cristian.rraga95@hotmail.com>
2023-01-30 13:27:08 -0500

    Resolviendo relaciones muchos a muchos

--- services/order.service.js
+++ services/order.service.js
@@ -22,6 +22,7 @@ class OrderService {
           association: 'customer',
           include: ['user'],
         },
+        'items'
       ],
     });
     if (!order) {
@@ -40,6 +41,11 @@ class OrderService {
     const order = await this.findOne(id);
     await order.destroy();
     return { id };
+  }
+
+  async addItem(data){
+    const newItem = await models.OrderProduct.create(data);
+    return newItem;
   }
 }
 
